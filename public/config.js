3bd6d38feb49612ae3ddd752bcbb5bec45f3b5da
yuyueshiwu <155555852+yuyueshiwu@users.noreply.github.com>
2024-01-05 22:08:18 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.__PRE_CONFIG__ = {
-  header: 'Server Status',
-  subHeader: 'Servers\' Probes Set up with ServerStatus',
+  header: 'Yu Yue',
+  subHeader: 'Welcome to the world designed by Yuyue',
   interval: 1.5,
   footer: '<p>Powered by <a href="https://github.com/cokemine/ServerStatus-Hotaru">ServerStatus-Hotaru</a></p>'
 };
